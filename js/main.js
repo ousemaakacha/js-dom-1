31f93d4f32a9e13c3fb306840495b91705266191
ousemaakacha <akachaousema@gmail.com>
2025-10-14 15:12:19 +0200

    wrote the function

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ console.log("lampadina");
 const img = document.getElementById("myImage");
 const img2 = document.getElementById("myImage2");
 const btnEl = document.getElementById("btnSW");
+
+// creo funzione per sostutuire le immagini al click del bottone
+
+function toggleLamp() {
+  img.classList.toggle("d-none");
+  img2.classList.toggle("d-none");
+}
