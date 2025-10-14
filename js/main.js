e4619c3294455400bdcd86681f5548ab9373f7e3
ousemaakacha <akachaousema@gmail.com>
2025-10-14 14:38:56 +0200

    added var to get the img from the html

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ console.log("lampadina");
 
 // cre una varibile per capire se la lampada é accesa o spenta
 let lampOn = false;
+
+// creo funzione per sostutuire le immagini al click del bottone
+
+function toggleLamp() {
+  const img = document.getElementById("myImage");
+}
