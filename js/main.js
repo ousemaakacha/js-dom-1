6a7e45067eb8fd13e3a662f182897c1d63f59d6e
ousemaakacha <akachaousema@gmail.com>
2025-10-15 09:51:03 +0200

    added the feature to change button from 'accendi ' to "spegni"
    PS: forgot to do it yesterday

--- js/main.js
+++ js/main.js
@@ -3,13 +3,17 @@ console.log("lampadina");
 const img = document.getElementById("myImage");
 const img2 = document.getElementById("myImage2");
 const btnEl = document.getElementById("btnSW");
+const btnEl2 = document.getElementById("btnSW2");
 
 // creo funzione per sostutuire le immagini al click del bottone
 
 function toggleLamp() {
   img.classList.toggle("d-none");
   img2.classList.toggle("d-none");
+  btnEl.classList.toggle("d-none");
+  btnEl2.classList.toggle("d-none");
 }
 
 // aggiungo event listener
 btnEl.addEventListener("click", toggleLamp);
+btnEl2.addEventListener("click", toggleLamp);
